6cb92ba3d62b876a24757148084c9f37e732217d
Karan Singh <1king.karanbanna@gmail.com>
2025-09-28 11:26:37 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,28 @@
+# light = "green"
+# if(light=="red"):
+#     print("stop")
+# elif(light=="green"):
+#     print("go")
+# elif(light=="yellow"):
+#     print("wait")
+# else:
+#     print("move on")
+
+# marks=int(input("enter the marks of student : "))
+# if(marks>=90):
+#     print("you have got grade :A")
+# elif(marks>=80 and marks<=90):
+#     print("you have got grade :B")
+# elif(marks>=70 and marks<=80):
+#     print("you have got grade : C")
+# else:
+#     print("you have got grade : D")
+
+# age  = int(input("enter your age :"))
+# if(age>=18):
+#     if(age>=80):
+#         print("you cannot drive")
+#     else:
+#         print("you can drive")
+# else:
+#     print("you cant drive")
